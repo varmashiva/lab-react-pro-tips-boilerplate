3e8622b7d5e1f0b129d732cfa60d01a699c083b1
Shiva Varma <shivavarma@Shivas-MacBook-Air.local>
2024-02-24 11:11:49 +0530

    added

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <div>
+            <div>
+                <h1>Kalvium ❤</h1>
+            </div>
+            <div>
+                <h1>Contacts</h1>
+                <h1>Registration Form</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
